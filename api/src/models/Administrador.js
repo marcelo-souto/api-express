f051df341f0d5e2cc1ea851e9e43b8a8b85d9dce
Marcelo Souto <marcelosouto676@gmail.com>
2023-02-18 15:37:11 -0300

    modelo administrador

---
+++ api/src/models/Administrador.js
@@ -0,0 +1,33 @@
+const sequelize = require('../database/database.js');
+const { DataTypes } = require('sequelize');
+
+const Administrador = sequelize.define(
+	'administradores',
+	{
+		administradorId: {
+			type: DataTypes.INTEGER,
+			primaryKey: true,
+			autoIncrement: true,
+			allowNull: false
+		},
+		nome: {
+			type: DataTypes.STRING,
+			allowNull: false
+		},
+		email: {
+			type: DataTypes.STRING,
+			allowNull: false
+		},
+		senha: {
+			type: DataTypes.STRING,
+			allowNull: false
+		}
+	},
+	{
+		timestamps: false
+	}
+);
+
+// Administrador.sync({ force: true });
+
+module.exports = Administrador;
