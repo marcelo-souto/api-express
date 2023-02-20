e776a4736ac43bf1ff1a1c0de166029779c973f1
Marcelo Souto <marcelosouto676@gmail.com>
2023-02-20 16:39:34 -0300

    rota de atualizacao do filme

--- api/src/controllers/filmeController.js
+++ api/src/controllers/filmeController.js
@@ -83,7 +83,7 @@ const filmeController = {
 					poster = {
 						url: link.join('/') + '/',
 						img: img,
-						alt: `Filme ${nome}`
+						alt: `Poster do filme ${nome}.`
 					};
 				}
 			}
@@ -95,7 +95,7 @@ const filmeController = {
 					poster = {
 						url: null,
 						img: file,
-						alt: `Filme ${nome}`
+						alt: `Poster do filme ${nome}.`
 					};
 				}
 			}
@@ -114,8 +114,7 @@ const filmeController = {
 
 			return res.status(200).json(filme);
 		} catch (erro) {
-
-			if (req.file) await unlink(req.file.path) 
+			if (req.file) await unlink(req.file.path);
 			return res.status(400).json({ erro: erro.message });
 		}
 	},
@@ -173,7 +172,141 @@ const filmeController = {
 				}
 			);
 
+			if (!filme)
+				return res.status(404).json({ erro: 'Filme não encontrado.' });
+
 			return res.status(200).json(filme);
+		} catch (erro) {
+			return res.status(400).json({ erro: erro.message });
+		}
+	},
+	update: async (req, res) => {
+		let {
+			administradorId: id,
+			filmeId,
+			nome,
+			sinopse,
+			dataEstreia,
+			poster,
+			disponivel3D: d3D,
+			disponivelLegendado: dLG,
+			generoId
+		} = req.body;
+
+		try {
+			// Checanco se o adm existe
+			const administrador = await Administrador.findByPk(id);
+			if (!administrador) throw new Error('Usuário não encontrado');
+
+			// Pegando o filme
+			const filme = await Filme.findByPk(filmeId);
+			if (!filme) throw new Error('Filme não encontrado.');
+
+			// Checando se o admin que solicitou mudanca é o mesmo que criou
+			if (filme.administradorId !== administrador.administradorId)
+				throw new Error('Você não tem permissão.');
+
+			// Validando os campos
+			validate({ sinopse });
+			validate({ 'data de estreia': dataEstreia, type: 'data' });
+			validate({ 'disponível em 3D': d3D, type: 'boolean' });
+			validate({ 'disponível legendado': dLG, type: 'boolean' });
+
+			// Checando se ja existe filme com o mesmo nome
+			if (nome) {
+				validate({ nome });
+				nome = nome.trim().toLowerCase();
+
+				const filmeJaExiste = await Filme.findOne({
+					where: { nome: `${nome}` }
+				});
+				if (filmeJaExiste) throw new Error('Filme já cadastrado.');
+			}
+
+			// Checando se o genero existe
+			if (generoId) {
+				validate({ 'id do genero': generoId, type: 'numero' });
+
+				const genero = await Genero.findByPk(generoId);
+				if (!genero) throw new Error('Genero não encontrado.');
+			}
+
+			// Checando se poster veio como link
+			if (poster && poster !== undefined) {
+				const validated = validate({ poster });
+				// Checar se a imagem existe
+				let response;
+				try {
+					response = await fetch(poster);
+					if (!response.ok) throw new Error();
+				} catch (error) {
+					if (error)
+						return res.status(400).json({ erro: 'Link de imagem invalido.' });
+				}
+
+				if (filme.poster.url === null) await unlink(`./public/media/${filme.poster.img}`)
+
+				if (validated) {
+					const link = poster.split('/');
+					const img = link.pop();
+					poster = {
+						url: link.join('/') + '/',
+						img: img,
+						alt: `Poster do filme ${filme.nome}.`
+					};
+				}
+			}
+
+			// Checando se veio como arquivo de imagem e fazendo tratamento
+			if (req.file) {
+				if (filme.poster.url === null) await unlink(`./public/media/${filme.poster.img}`)
+
+				const file = await imageUploadHandle(req.file);
+				if (file) {
+					poster = {
+						url: null,
+						img: file,
+						alt: `Poster do filme ${filme.nome}.`
+					};
+				}
+			}
+
+			// Criando o novo filme
+			const filmeAtualizado = await filme.update({
+				nome: nome ? nome : filme.nome,
+				sinopse: sinopse ? sinopse : filme.sinopse,
+				dataEstreia: dataEstreia ? dataEstreia : filme.dataEstreia,
+				poster: poster ? poster : filme.poster,
+				disponivel3D: d3D ? d3D : filme.disponivel3D,
+				disponivelLegendado: dLG ? dLG : filme.disponivelLegendado,
+				generoId: generoId ? generoId : filme.generoId
+			});
+
+			return res.status(200).json(filmeAtualizado);
+		} catch (erro) {
+
+			if (req.file) await unlink(req.file.path);
+			return res.status(400).json({ erro: erro.message });
+		}
+	},
+	delete: async (req, res) => {
+		const { administradorId: id } = req.body;
+		const { filmeId } = req.params;
+
+		try {
+			const administrador = await Administrador.findByPk(id);
+			if (!administrador)
+				return res.status(401).json({ erro: 'Você nao tem permissão.' });
+
+			const filme = await Filme.findByPk(filmeId);
+			if (!filme)
+				return res.status(404).json({ erro: 'Filme não encontrado.' });
+
+			const filmeDeletado = await filme.destroy();
+
+			return res.status(200).json({
+				mensagem: `Filme ${filmeDeletado.nome} foi excluido com sucesso.`
+			});
 		} catch (erro) {
 			return res.status(400).json({ erro: erro.message });
 		}
--- api/src/routes/filmeRoutes.js
+++ api/src/routes/filmeRoutes.js
@@ -6,7 +6,8 @@ const upload = require('../middlewares/upload.js');
 const router = Router();
 
 router.post('/filme/create', checkToken, upload, filmeController.create);
-
+router.delete('/filme/delete/:filmeId', checkToken, filmeController.delete)
+router.put('/filme/update', checkToken, upload, filmeController.update)
 
 router.get('/filme/get', filmeController.getAll);
 router.get('/filme/get/:filmeId', filmeController.getById);
